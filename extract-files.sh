c9d111ce78df8b7b67fd2e6e59a40467a96f97b1
tyypeshi1 <3mkyad33@gmail.com>
2025-08-03 21:09:30 +0300

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -12,9 +12,8 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+ANDROID_ROOT="$(realpath "${MY_DIR}/../../..")"
+HELPER="tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -89,8 +88,10 @@ function blob_fixup() {
 
 if [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper for common device
-    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
-
+    # Use home directory for vendor files
+SAFE_ROOT="$HOME/android_safe"
+mkdir -p "${SAFE_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${SAFE_ROOT}" true "${CLEAN_VENDOR}"
     extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
